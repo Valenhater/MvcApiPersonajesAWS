224f16eb7b22d439ccc9fc2530822222ac79c257
Valenhater <xnitezl@gmail.com>
2024-05-17 11:40:11 +0200

    Add project files.

---
+++ MvcApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPersonajesAWS.Models;
+using MvcApiPersonajesAWS.Services;
+
+namespace MvcApiPersonajesAWS.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        private ServiceApiPersonajes service;
+
+        public PersonajesController(ServiceApiPersonajes service)
+        {
+            this.service = service;
+        }
+    
+        public async Task<IActionResult> Index()
+        {
+            List<Personaje> personajes = await this.service.GetPersonajesAsync();
+            return View(personajes);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Personaje personaje)
+        {
+            await this.service.CreatePersonajeAsync(personaje.Nombre, personaje.Imagen);
+            return RedirectToAction("Index");
+        }
+    }
+}
